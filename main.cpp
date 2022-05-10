b804b84a57846ed6b728f4853264dc602b4f15d7
li_heng <liheng@MacBook-Pro.local>
2022-05-11 00:52:04 +0800

    add some

--- main.cpp
+++ main.cpp
@@ -7,6 +7,14 @@
 using namespace std; // 命名空间
 
 //设计模式
+// 1. 工厂模式
+class Product
+{
+public:
+    virtual ~Product(); //析构函数
+protected:
+    Product();
+};
 int main() {
     cout << "Hello, World!" << endl;
     return 0;
