d8af87f6794cb50635e78fa92aea376b5a6ffc76
li_heng <1033797938@qq.com>
2022-05-12 16:00:18 +0800

    add some

--- c++pro.cpp
+++ c++pro.cpp
@@ -20,7 +20,7 @@ class Product
 public:
     virtual void showPro() = 0;
 };
-class ProductA : public Product
+class ProductA: public Product
 {
 public:
     void showPro()
@@ -28,7 +28,71 @@ public:
         cout << "I'm ProductA" << endl;
     }
 };
+
+class ProductB: public Product
+{
+public:
+    void showPro()
+    {
+        cout << "I'm ProductB" << endl;
+    }
+};
+
+class ProductC : public Product
+{
+public:
+    void showPro()
+    {
+        cout << "I'm ProductC" << endl;
+    }
+};
+
+class Factory
+{
+public:
+    Product* CreateProduct(PRODUCTTYPE type)
+    {
+        switch(type)
+        {
+        case TypeA:
+            return new ProductA();
+        case TypeB:
+            return new ProductB();
+        case TypeC:
+            return new ProductC();
+        default:
+            return NULL;
+        }
+    }
+};
 int main()
 {
+    Factory* ProductFactory = new Factory();
+    Product* productObjA = ProductFactory->CreateProduct(TypeA);
+    if(productObjA != NULL)
+    {
+        productObjA->showPro();
+    }
+    Product* productObjB = ProductFactory->CreateProduct(TypeB);
+    if(productObjB != NULL)
+    {
+        productObjB->showPro();
+    }
+    Product* productObjC = ProductFactory->CreateProduct(TypeC);
+    if(productObjC != NULL)
+    {
+        productObjC->showPro();
+    }
+    delete ProductFactory;
+    ProductFactory = NULL;
+
+    delete productObjA;
+    ProductFactory = NULL;
+
+    delete productObjB;
+    ProductFactory = NULL;
+
+    delete productObjC;
+    ProductFactory = NULL;
     cout << "Hello World!\n";
 }
